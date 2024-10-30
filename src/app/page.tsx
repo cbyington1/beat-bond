df7d7b2ef335d100a8ea5ffdf7db0056a8beaa68
Hersh Lalwani <Hersh.r.lalwani@gmail.com>
2024-10-30 18:33:28 -0500

    added rec and playlist pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,24 @@ import Image from "next/image";
 import React from "react";
 import { Card } from "@/components/ui/card";
 import { useUser } from "@clerk/nextjs";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
 
   const { isLoaded, isSignedIn, user } = useUser()
-
-  if (!isLoaded || !isSignedIn) {
+  if (!isLoaded) {
+    return (
+      <div className="h-full w-full py-2 pr-2">
+        <Card className="h-full w-full bg-bbbackground text-[#FFFFFF] text-2xl">
+          <div className="p-4">
+            <Image src='/bblogo.jpeg' alt="bblogo" width={150} height={150} className="rounded-xl"></Image>
+            <Skeleton className="h-7 w-7 rounded-full"></Skeleton>
+            <Skeleton className='h-4 w-[150px] self-center'></Skeleton>
+          </div>
+        </Card>
+      </div>
+    )
+  } else if (!isSignedIn) {
       return (
         <div className="h-full w-full py-2 pr-2">
         <Card className="h-full w-full bg-bbbackground text-[#FFFFFF] text-2xl">
@@ -22,7 +34,6 @@ export default function Home() {
   }
 
   const username = user.firstName
-
   return (
     <div className="h-full w-full py-2 pr-2">
       <Card className="h-full w-full bg-bbbackground text-[#FFFFFF] text-2xl">
--- src/app/recommendations/page.tsx
+++ src/app/recommendations/page.tsx
@@ -1,11 +1,19 @@
-
+'use client'
+import Image from "next/image";
+import React from "react";
+import { Card } from "@/components/ui/card";
 
 export default function recommendations() {
 
     return (
-      <div>
-        
-      </div>
+      <div className="h-full w-full py-2 pr-2">
+      <Card className="h-full w-full bg-bbbackground text-[#FFFFFF] text-2xl">
+        <div className="p-4">
+          <Image src='/bblogo.jpeg' alt="bblogo" width={150} height={150} className="rounded-xl"></Image>
+          <p>Recommendations</p>
+        </div>
+      </Card>
+    </div>
     );
   }
   
